a031ce3e34cbb0b7e8eab3ca4ef45897299796f4
mrsaeeddev <saeed_dev@yahoo.com>
2021-10-10 15:35:48 +0500

    completed listing api call

--- project/librarie/src/components/BooksList.js
+++ project/librarie/src/components/BooksList.js
@@ -2,10 +2,26 @@ import React from 'react';
 import NavBar from './NavBar';
 
 function BooksList() {
-    return (<div>
+    const [booksList, setBooksList] = React.useState([]);
+
+    React.useEffect(() => {
+        let url = "https://my-json-server.typicode.com/dmitrijt9/book-api-mock/books"
+        fetch(url)
+        .then(response => response.json())
+        .then(resp => setBooksList(resp))
+    },[]);
+
+    return (
+    <div>
         <NavBar />
-        <div>list</div>
-    </div>)
+        <div>{booksList.map((v,i)=>{
+            return (<div>
+                    <div>{v.title}</div>
+                    <div>{v.releaseDate}</div>
+                    </div>)
+        })}</div>
+    </div>
+    )
 }
 
 export default BooksList;
